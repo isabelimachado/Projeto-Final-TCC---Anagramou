573a12c2a28aa68640636de0e89c56e1b6e831b5
isa <isabelisantos0616@gmail.com>
2025-08-06 08:37:35 -0300

    Update script.js

--- TCC PROJETO BRAYAN E EU/script.js
+++ TCC PROJETO BRAYAN E EU/script.js
@@ -1,12 +1,12 @@
 function alternarTema() {
-    const body = document.body;
-    const iconeTema = document.querySelector('.botao-tema i');
+    const body = document.body; //contsante do body pq vai ser alteradi
+    const iconeTema = document.querySelector('.botao-tema i');//pra indicar qual o icone do botao
     
-    if (body.getAttribute('data-theme') === 'dark') {
-        body.removeAttribute('data-theme');
-        iconeTema.className = 'fas fa-moon';
+    if (body.getAttribute('data-theme') === 'dark') { //se o body tem os dados do tema escuro -> ativo
+        body.removeAttribute('data-theme'); //tirar os atributos do tema escuro , pra volta pro claro
+        iconeTema.className = 'fas fa-moon'; //adiciona no icone o simbolo da lua pra dizer que ta no claro e pd mudar
     } else {
-        body.setAttribute('data-theme', 'dark');
+        body.setAttribute('data-theme', 'dark'); //senao,tema escuro e icone do sol q eh pra volta pro claro
         iconeTema.className = 'fas fa-sun';
     }
 }
