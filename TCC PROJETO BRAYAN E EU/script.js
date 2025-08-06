dde4127e1cf706c34f29259edc017ee590d2281f
isa <isabelisantos0616@gmail.com>
2025-08-06 13:05:33 -0300

    Update script.js

--- TCC PROJETO BRAYAN E EU/script.js
+++ TCC PROJETO BRAYAN E EU/script.js
@@ -1,12 +1,21 @@
-function alternarTema() {
-    const body = document.body; //contsante do body pq vai ser alteradi
-    const iconeTema = document.querySelector('.botao-tema i');//pra indicar qual o icone do botao
-    
-    if (body.getAttribute('data-theme') === 'dark') { //se o body tem os dados do tema escuro -> ativo
-        body.removeAttribute('data-theme'); //tirar os atributos do tema escuro , pra volta pro claro
-        iconeTema.className = 'fas fa-moon'; //adiciona no icone o simbolo da lua pra dizer que ta no claro e pd mudar
-    } else {
-        body.setAttribute('data-theme', 'dark'); //senao,tema escuro e icone do sol q eh pra volta pro claro
-        iconeTema.className = 'fas fa-sun';
-    }
-}
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyByESGl7b8-X74bPX3GXpArf5SixfEQ_Ew",
+      authDomain: "anagramou.firebaseapp.com",
+      projectId: "anagramou",
+      storageBucket: "anagramou.firebasestorage.app",
+      messagingSenderId: "518755435289",
+      appId: "1:518755435289:web:b33e54e698718914323b88",
+      measurementId: "G-BBQX4DEE5V"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
