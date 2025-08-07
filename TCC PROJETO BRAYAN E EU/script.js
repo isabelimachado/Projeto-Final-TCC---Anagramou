c7329992b0f3e2b217dea2dc8f5bab1975ac0fef
isa <isabelisantos0616@gmail.com>
2025-08-07 13:31:55 -0300

    coisas haha

---
+++ TCC PROJETO BRAYAN E EU/script.js
@@ -0,0 +1,4 @@
+// fazer uma função para enviar os dados do usuario para o banco e mostra no rank
+//tb uma função de login e outra eu acho pra mostra o perfil do usuario e talvez o maior time dele
+function enviarDadosRegistro() {
+  }
