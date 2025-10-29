b8f73d5faace02a43061836918382133fa29d16a
Brayan Freitas Dierings <brayanloopy@gmail.com>
2025-10-29 14:35:12 -0300

    odeio a api do gemini

--- ArquivosPrincipais/pastarParaTestarFunções/testeJS.js
+++
@@ -1,5 +0,0 @@
-    const stringTeste = "10/10/2020"
-    const resultado = stringTeste.split("/")[0]
-
-    console.log("dia:"+resultado)
-    console.log(resultado)
--- ArquivosPrincipais/testeJS.js
+++ ArquivosPrincipais/testeJS.js
@@ -1,80 +1,63 @@
-const serviceAccount = require('./seguranca.json');
-const {GoogleGenAI}  = require("@google/genai")
 require ('dotenv').config({ path: "./variaveis.env"})
-dotenv.config({ path: "./variaveis.env" });
-
-const ai = new GoogleGenAI({
-  apiKey:process.env.GOOGLE_API_KEY,
-});
-
 console.log(process.env.GOOGLE_API_KEY)
 
-/* require (`GoogleGenAI` ) from "@google/genai"; */
-/* import dotenv from "dotenv" */
-
-/* async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
-} */
-
+const serviceAccount = require('./seguranca.json')
+const admin = require('firebase-admin');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const hoje = new Date();
-const diaAtual = new Intl.DateTimeFormat('pt-BR')
-  .format(hoje)
-  .replace(/\//g, '-');  
 
-
-const db = admin.firestore();
-
-async function exemplo(a1, a2, a3, a4, a5, a6,database) {
-  let banco = ""
-  if(database == 1) banco = "descPalavraFacil"  
-  if(database == 2) banco = "descPalavraMedia"
-  if(database == 3) banco = "descPalavraDificil"
-const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash-lite"
+const {GoogleGenAI}  = require("@google/genai")
+const ai = new GoogleGenAI({
+  apiKey:process.env.GOOGLE_API_KEY,
 });
 
-  const palavras = [a1, a2, a3, a4, a5, a6];
-  const prompt = `Forneça apenas um sinônimo simples e moderno para as seguintes palavras, na mesma ordem, podendo colocar uma ou duas palavras por item(preferivel duas), separados por vírgula. Não adicione explicações, quebras de linha, nem texto extra. Apenas os sinônimos: ${palavras.join(", ")}`;
 
-  
-  try {
-    const result = await model.generateContent(prompt);
-    const texto = result.response.text(); 
-    
-    const sinonimos = texto.split(",").map(s => s.trim());
+const hoje = new Date();
+const diaAtual = new Intl.DateTimeFormat('pt-BR') .format(hoje).replace(/\//g, '-');  
+const db = admin.firestore();
 
-    console.log("sinonimos gerados:", sinonimos);
+async function exemplo(a1,a2,a3,a4,a5,a6,database)
+{
+    let banco = ""
+    if(database == 1) banco = "descPalavraFacil"  
+    if(database == 2) banco = "descPalavraMedia"
+    if(database == 3) banco = "descPalavraDificil"
+   const palavras = [a1,a2,a3,a4,a5,a6] 
+    try{
+         const resposta = await ai.models.generateContent({
+          model: "gemini-2.0-flash",
+          contents : `Forneça apenas um sinônimo simples e moderno para as seguintes palavras, na mesma ordem, com exatamente uma palavras ou duas por item, separados por vírgula. Não adicione explicações, quebras de linha, nem texto extra. Apenas os sinônimos: ${palavras.join(", ")}`,
+         })
+         const texto = resposta.text
+         console.log(texto)
 
-    const docRef = db.collection(banco).doc(diaAtual);
-    const docSnap = await docRef.get();
+        const sinonimos = texto.split(",").map(s => s.trim());
 
-    if (docSnap.exists) {
-      console.log("Existe uma referência hoje");
-      return;
-    }
+        console.log("sinonimos gerados:", sinonimos);
 
-    await docRef.set({
-      resumo1: sinonimos[0] || "",
-      resumo2: sinonimos[1] || "",
-      resumo3: sinonimos[2] || "",
-      resumo4: sinonimos[3] || "",
-      resumo5: sinonimos[4] || "",
-      resumo6: sinonimos[5] || ""
-    });
+        const docRef = db.collection(banco).doc(diaAtual);
+        const docSnap = await docRef.get();
+
+        if (docSnap.exists) {
+          console.log("Existe uma referência hoje");
+          return;
+        }
+
+        await docRef.set({
+          resumo1: sinonimos[0] || "",
+          resumo2: sinonimos[1] || "",
+          resumo3: sinonimos[2] || "",
+          resumo4: sinonimos[3] || "",
+          resumo5: sinonimos[4] || "",
+          resumo6: sinonimos[5] || ""
+        });
 
     console.log("salvos!");
-
-  } catch (err) {
-    console.error("Erro!:", err);
-  }
+    }catch(erro){
+        console.log(erro)
+    }
 }
 
 
@@ -144,6 +127,9 @@ async function procurarDadosDificil() {
     }
 }
 
-procurarDadosFaceis();
-procurarDadosMedio();
-procurarDadosDificil(); 
+/* procurarDadosFaceis() */
+
+
+procurarDadosFaceis()
+procurarDadosMedio()
+procurarDadosDificil()
